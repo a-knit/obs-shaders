1a2cbf4c6950ad80dd617101b7201c780f2073be
Alex Knittel <alexlknittel@gmail.com>
2026-02-21 14:40:02 -0800

    add cartoon original

--- src/cartoon.glsl
+++ src/cartoon.glsl
@@ -1,10 +1,3 @@
-/*
- * "Toon webcam" by Ben Wheatley - 2018
- * License MIT License
- * Contact: github.com/BenWheatley
- */
-
-
 vec3 posterize(vec3 col, int rCount, int gCount, int bCount) {
     float fColR = float(rCount);
     float fColG = float(gCount);
@@ -17,12 +10,9 @@ vec3 posterize(vec3 col, int rCount, int gCount, int bCount) {
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    // float time = iTime;
     float time = builtin_elapsed_time;
-    // vec2 uv = fragCoord.xy / iResolution.xy;
     vec2 uv = fragCoord.xy;
     
-    // vec2 pixelSize = vec2(1,1) / iResolution.xy;
     vec2 pixelSize = vec2(1,1) / builtin_uv_size;
     
     vec3 sum = vec3(0,0,0);
@@ -35,14 +25,12 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 	    for (int dx = -1; dx<=1; ++dx) {
             vec2 pixelOff = pixelSize * vec2(dx, dy);
             vec2 tex_uv = uv + pixelOff;
-            // vec3 textureValue = texture(iChannel0, tex_uv).rgb;
             vec3 textureValue = image.Sample(builtin_texture_sampler, tex_uv).rgb;
             sum += (kernel[dx+1][dy+1] * textureValue);
         }
     }
     
     vec3 edge = sum;
-    // vec3 cam = posterize(texture(iChannel0, uv).rgb, 4, 4, 2);
     vec3 cam = posterize(image.Sample(builtin_texture_sampler, uv).rgb, 4, 4, 2);
     
     fragColor = vec4(cam + 5.0*edge,1.);
